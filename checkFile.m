7d3305007a6ffbfae8d7a115e0832761084f62e9
Adam Li <adam2392@gmail.com>
2014-08-03 13:04:07 -0700

    Fixed Reanimate Skeleton
    Full Functionality… now trying to add checks to mark images as bad.
    
    Will also change saving figures to increase speed.

---
+++ checkFile.m
@@ -0,0 +1,13 @@
+%Function written by Adam Li on 08/02/14
+%Copyright: Adam Li
+%
+%Input: - fileName is the file name to check
+%       
+%
+%Output: - either 1 if the file is not bad, or
+%                 0 if the file is bad
+%Description: A helpfer function that gets passed a filename that checks if
+%the file is bad or not, by parsing for the string 'bad'
+%**Will add some sort of check in main function that calls on this to see if 
+%   there is a string of "bad files"
+
